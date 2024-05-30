bbb510a208fc6d64b3674d0799d9eca82ccc92bb
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2024-05-30 14:40:55 +0200

    Add unit test for ListDependenciesRecursive function

---
+++ graph/recursive_test.go
@@ -0,0 +1,31 @@
+package graph
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/charmbracelet/log"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestListDependenciesRecursive(t *testing.T) {
+	fs := afero.NewMemMapFs()
+	var buf bytes.Buffer
+	logger := log.NewWithOptions(&buf, log.Options{})
+	dependencies := map[string][]string{
+		"A": {"B", "C"},
+		"B": {"D"},
+		"C": {"D"},
+		"D": {},
+	}
+	dg := NewDependencyGraph(fs, logger, dependencies)
+	visited := make(map[string]bool)
+	output := captureOutput(func() {
+		dg.ListDependenciesRecursive("A", []string{}, visited)
+	})
+
+	expectedOutput := "A <- B <- D\nA <- C <- D\n"
+
+	assert.Equal(t, expectedOutput, output)
+}
