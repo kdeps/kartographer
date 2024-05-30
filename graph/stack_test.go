aca0bf24cc644845578d25b83718ec60c0d1a65b
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2024-05-30 14:42:33 +0200

    Add tests for stack implementation

---
+++ graph/stack_test.go
@@ -0,0 +1,28 @@
+package graph
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/charmbracelet/log"
+	"github.com/spf13/afero"
+)
+
+func TestBuildDependencyStack(t *testing.T) {
+	fs := afero.NewMemMapFs()
+	var buf bytes.Buffer
+	logger := log.NewWithOptions(&buf, log.Options{})
+	dependencies := map[string][]string{
+		"A": {"B", "C"},
+		"B": {"D"},
+		"C": {"D"},
+		"D": {},
+	}
+	dg := NewDependencyGraph(fs, logger, dependencies)
+	visited := make(map[string]bool)
+	stack := dg.BuildDependencyStack("A", visited)
+	expected := []string{"D", "B", "C", "A"}
+	if !equal(stack, expected) {
+		t.Errorf("Expected %v, got %v", expected, stack)
+	}
+}
