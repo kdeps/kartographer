eaf102c53d29fe2225b8f83a788f977d51415dd9
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2025-09-01 13:02:06 +0200

    Refactor TraverseDependencyGraph to meet 15-line limit

--- graph/traverse.go
+++ graph/traverse.go
@@ -4,22 +4,30 @@ func (dg *DependencyGraph) TraverseDependencyGraph(node string, dependencies map
 	if visited[node] {
 		return
 	}
+	if !dg.prepareTraversal(node, visited) {
+		return
+	}
+	dg.processNodeDependencies(node, dependencies, visited)
+	dg.DependencyGraph = dg.DependencyGraph[:len(dg.DependencyGraph)-1]
+}
+
+func (dg *DependencyGraph) prepareTraversal(node string, visited map[string]bool) bool {
 	visited[node] = true
 	dg.DependencyGraph = append(dg.DependencyGraph, node)
-
 	currentPath := dg.ConstructDependencyPath(dg.DependencyGraph, "forward")
 	if dg.VisitedPaths[currentPath] {
 		dg.DependencyGraph = dg.DependencyGraph[:len(dg.DependencyGraph)-1]
-		return
+		return false
 	}
-
 	dg.VisitedPaths[currentPath] = true
 	dg.PrintDependencyPath(currentPath)
+	return true
+}
 
+func (dg *DependencyGraph) processNodeDependencies(node string, dependencies map[string][]string, visited map[string]bool) {
 	if deps, exists := dependencies[node]; exists {
 		for _, dep := range deps {
 			dg.TraverseDependencyGraph(dep, dependencies, visited)
 		}
 	}
-	dg.DependencyGraph = dg.DependencyGraph[:len(dg.DependencyGraph)-1]
 }
