ebe1b53b5c0eade823a8e10ecd5c6628cfe88ec7
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2025-09-01 13:02:06 +0200

    Integrate clean architecture layers with dependency injection

--- graph/graph.go
+++ graph/graph.go
@@ -3,6 +3,9 @@ package graph
 import (
 	"github.com/charmbracelet/log"
 	"github.com/spf13/afero"
+	"github.com/kdeps/kartographer/graph/domain"
+	"github.com/kdeps/kartographer/graph/infrastructure"
+	"github.com/kdeps/kartographer/graph/usecase"
 )
 
 type DependencyGraph struct {
@@ -11,13 +14,22 @@ type DependencyGraph struct {
 	DependencyGraph  []string
 	VisitedPaths     map[string]bool
 	Logger           *log.Logger
+	depService       domain.DependencyService
+	pathService      domain.PathService
 }
 
 func NewDependencyGraph(fs afero.Fs, logger *log.Logger, dependencies map[string][]string) *DependencyGraph {
+	repo := infrastructure.NewInMemoryGraphRepository(dependencies)
+	formatter := infrastructure.NewArrowPathFormatter()
+	writer := infrastructure.NewLoggerOutputWriter(logger)
+	pathService := usecase.NewPathService(formatter, writer)
+	depService := usecase.NewDependencyService(repo, pathService)
 	return &DependencyGraph{
 		Fs:               fs,
 		NodeDependencies: dependencies,
 		VisitedPaths:     make(map[string]bool),
 		Logger:           logger,
+		depService:       depService,
+		pathService:      pathService,
 	}
 }
