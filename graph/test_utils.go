2e4d9d277d40cbd3a23ba45acd911240aa6f4b9b
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2025-09-01 13:02:06 +0200

    Refactor captureOutput function to meet 15-line limit

--- graph/test_utils.go
+++ graph/test_utils.go
@@ -8,26 +8,37 @@ import (
 
 // captureOutput captures the output of a function for testing
 func captureOutput(f func()) string {
+	r, w := createPipe()
+	old := redirectStdout(w)
+	outC := startReader(r)
+	f()
+	w.Close()
+	os.Stdout = old
+	return <-outC
+}
+
+func createPipe() (*os.File, *os.File) {
 	r, w, err := os.Pipe()
 	if err != nil {
 		panic(err)
 	}
+	return r, w
+}
 
+func redirectStdout(w *os.File) *os.File {
 	old := os.Stdout
 	os.Stdout = w
+	return old
+}
 
+func startReader(r *os.File) chan string {
 	outC := make(chan string)
 	go func() {
 		var buf bytes.Buffer
 		io.Copy(&buf, r)
 		outC <- buf.String()
 	}()
-
-	f()
-
-	w.Close()
-	os.Stdout = old
-	return <-outC
+	return outC
 }
 
 func equal(a, b []string) bool {
